0d9372eb1edb513a1750fcdbb692501a78d4a5c3
dwilliams64 <dwilliams_64@yahoo.com>
2019-01-23 18:52:29 -0600

    Completed build out of basic components

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,7 @@ import brain from './smartbrain.gif';
 const Logo = () => {
     return(
         <div className='ma4 mt0'>
-            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 200, width: 200 }} >
+            <Tilt className="Tilt br2 shadow-4" options={{ max : 55 }} style={{ height: 200, width: 200 }} >
                 <div className="Tilt-inner pv4"><img alt='logo' src={brain}></img></div>
             </Tilt>
         </div>
